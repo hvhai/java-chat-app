a7a6bc6a4f3e28c306317bcd3e6171af7468c91d
hvhai <hunterx6323@gmail.com>
2024-03-24 21:38:38 +0700

    feat: send message from client to server

--- app/src/main/java/com/codehunter/java_chat_app/client/ChatClient.java
+++ app/src/main/java/com/codehunter/java_chat_app/client/ChatClient.java
@@ -1,17 +1,31 @@
 package com.codehunter.java_chat_app.client;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 class ChatClient {
+
     public static final Logger log = LogManager.getLogger(ChatClient.class);
-    
+
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 2000)) {
-            log.info("connected");
+        try (
+            Socket socket = new Socket("localhost", 2000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());) {
+                log.info("connected");
+                String line = "";
+                while (!"exit".equals(line)) {
+                    line = in.readLine();
+                    log.info("input line: {}", line);
+                    writer.println(line);
+                }
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e);
         }
     }
 }
--- app/src/main/java/com/codehunter/java_chat_app/server/ChatServer.java
+++ app/src/main/java/com/codehunter/java_chat_app/server/ChatServer.java
@@ -1,27 +1,59 @@
 package com.codehunter.java_chat_app.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-
 class ChatServer {
+
     public static final Logger log = LogManager.getLogger(ChatServer.class);
-    
+    private static final List<ClientHandler> clients = new ArrayList<ClientHandler>();
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(2000)) {
             log.info("Server started, waiting for connection");
-            Socket socket = serverSocket.accept();
-            log.info("Client connected {}", socket.getInetAddress().getCanonicalHostName());
+            while (true) {
+                Socket socket = serverSocket.accept();
+                log.info("Client connected {}", socket.getInetAddress().getCanonicalHostName());
+                ClientHandler clientThread = new ClientHandler(socket);
+                clients.add(clientThread);
+
+                new Thread(clientThread).start();
+            }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e);
         }
-        
+
+    }
+
+    private static class ClientHandler implements Runnable {
+
+        private static final Logger log = LogManager.getLogger(ClientHandler.class);
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            log.info("Client handler is running");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null && !inputLine.equals("null")) {
+                    log.info(inputLine);
+                }
+            } catch (Exception e) {
+                log.error(e);
+            }
+        }
     }
 
 }
